ef693f8916a4327b498a4757220790bfd2bf37de
compsysadmin <1412244189@qq.com>
2018-09-15 08:43:06 +0800

    新增“先到先服务”模型

--- runRGV.cpp
+++ runRGV.cpp
@@ -1,5 +1,6 @@
 // runRGV.cpp : 定义控制台应用程序的入口点。
 //
+#pragma once
 
 #include "stdafx.h"
 #include <iostream>
@@ -10,8 +11,42 @@ using namespace std;
 int main()
 {
 	Rgv *rgv = new Rgv(20, 33, 46, 28, 31, 25);
-	rgv->pull();
 	Cnc *cnc1 = new Cnc(560, rgv);
+	Cnc *cnc2 = new Cnc(560, rgv);
+	Cnc *cnc3 = new Cnc(560, rgv);
+	Cnc *cnc4 = new Cnc(560, rgv);
+	Cnc *cnc5 = new Cnc(560, rgv);
+	Cnc *cnc6 = new Cnc(560, rgv);
+	Cnc *cnc7 = new Cnc(560, rgv);
+	Cnc *cnc8 = new Cnc(560, rgv);
+
+	for (int systemTime = 0; systemTime < 28800; systemTime++) {
+		cout << "System Time: " << systemTime << "s" << endl;
+
+		cnc1->onSystemTimeChange(systemTime);
+		cnc2->onSystemTimeChange(systemTime);
+		cnc3->onSystemTimeChange(systemTime);
+		cnc4->onSystemTimeChange(systemTime);
+		cnc5->onSystemTimeChange(systemTime);
+		cnc6->onSystemTimeChange(systemTime);
+		cnc7->onSystemTimeChange(systemTime);
+		cnc8->onSystemTimeChange(systemTime);
+		rgv->onSystemTimeChange(systemTime);
+
+		cout << endl;
+	}
+
+	cout << "\n\n最终加工的零件个数为：" << rgv->sum << "个\n\n" << endl;
+
+	delete cnc1;
+	delete cnc2;
+	delete cnc3;
+	delete cnc4;
+	delete cnc5;
+	delete cnc6;
+	delete cnc7;
+	delete cnc8;
+	delete rgv;
     return 0;
 }
 
