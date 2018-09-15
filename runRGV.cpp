7de81c7d160d2fe6f1c7c9ba7205c60928d749fb
compsysadmin <1412244189@qq.com>
2018-09-16 05:35:23 +0800

    一道工序的全部代码及仿真数据

--- runRGV.cpp
+++ runRGV.cpp
@@ -4,21 +4,34 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <windows.h>
+#include <time.h>
+#include <fstream>
 #include "Rgv.h"
 #include "Cnc.h"
 using namespace std;
 
-int main()
-{
+int main() {
+	// 第一组
 	Rgv *rgv = new Rgv(20, 33, 46, 28, 31, 25);
-	Cnc *cnc1 = new Cnc(560, rgv);
-	Cnc *cnc2 = new Cnc(560, rgv);
-	Cnc *cnc3 = new Cnc(560, rgv);
-	Cnc *cnc4 = new Cnc(560, rgv);
-	Cnc *cnc5 = new Cnc(560, rgv);
-	Cnc *cnc6 = new Cnc(560, rgv);
-	Cnc *cnc7 = new Cnc(560, rgv);
-	Cnc *cnc8 = new Cnc(560, rgv);
+	int probability = 0, processTime = 560;
+
+	// 第二组
+	/*Rgv *rgv = new Rgv(23, 41, 59, 30, 35, 30);
+	int probability = 1, processTime = 580;*/
+
+	// 第三组
+	/*Rgv *rgv = new Rgv(18, 32, 46, 27, 32, 25);
+	int probability = 1, processTime = 545;*/
+
+	Cnc *cnc1 = new Cnc(processTime, probability, rgv);
+	Cnc *cnc2 = new Cnc(processTime, probability, rgv);
+	Cnc *cnc3 = new Cnc(processTime, probability, rgv);
+	Cnc *cnc4 = new Cnc(processTime, probability, rgv);
+	Cnc *cnc5 = new Cnc(processTime, probability, rgv);
+	Cnc *cnc6 = new Cnc(processTime, probability, rgv);
+	Cnc *cnc7 = new Cnc(processTime, probability, rgv);
+	Cnc *cnc8 = new Cnc(processTime, probability, rgv);
 
 	for (int systemTime = 0; systemTime < 28800; systemTime++) {
 		cout << "System Time: " << systemTime << "s" << endl;
@@ -36,7 +49,8 @@ int main()
 		cout << endl;
 	}
 
-	cout << "\n\n最终加工的零件个数为：" << rgv->sum << "个\n\n" << endl;
+	cout << "\n\n最终加工的零件个数为：" << rgv->sumDone << "个\n\n" << endl;
+	cout << "\n\n最终报废的零件个数为：" << cnc1->sumDuang + cnc2->sumDuang + cnc3->sumDuang + cnc4->sumDuang + cnc5->sumDuang + cnc6->sumDuang + cnc7->sumDuang + cnc8->sumDuang << "个\n\n" << endl;
 
 	delete cnc1;
 	delete cnc2;
@@ -47,6 +61,12 @@ int main()
 	delete cnc7;
 	delete cnc8;
 	delete rgv;
+
+	while(true) {	// 用于在运行结束后响铃吵醒用户查看数据
+		Beep(1000,80);
+		Sleep(80);
+	}
+
     return 0;
 }
 
